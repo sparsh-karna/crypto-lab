a7d7013652b6506a060d68bd4a05d78d1aab5cb6
Sparsh Karna <sparsh2005karna@gmail.com>
2025-12-18 09:42:31 +0530

    report pattially done

--- lab1/ceaser/Server.java
+++ lab1/ceaser/Server.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Server {
 
-    static String decrypt(String s, int n) {
+    static String encrypt(String s, int n) {
         String new_s = "";
         for (char c : s.toCharArray()) {
             if (c >= 'a' && c <= 'z') {
-                new_s += (char) ((c - 'a' - n + 26) % 26 + 'a');
+                new_s += (char) ((c - 'a' + n) % 26 + 'a');
             } else if (c >= 'A' && c <= 'Z') {
-                new_s += (char) ((c - 'A' - n + 26) % 26 + 'A');
+                new_s += (char) ((c - 'A' + n) % 26 + 'A');
             } else {
                 new_s += c;
             }
@@ -19,24 +20,31 @@ public class Server {
 
     public static void main(String[] args) {
         try {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter message: ");
+            String message = input.nextLine();
+
+            System.out.print("Enter key: ");
+            int n = input.nextInt();
+
+            String encrypted = encrypt(message, n);
+
             ServerSocket serverSocket = new ServerSocket(5000);
-            System.out.println("Server started. Waiting for message...");
+            System.out.println("Server started. Waiting for client...");
 
             Socket socket = serverSocket.accept();
             System.out.println("Client connected.");
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            String encrypted = in.readLine();
-            int key = Integer.parseInt(in.readLine());
+            out.println(encrypted);
 
-            System.out.println("Encrypted message received: " + encrypted);
-
-            String decrypted = decrypt(encrypted, key);
-            System.out.println("Decrypted (original) message: " + decrypted);
+            System.out.println("Encrypted message sent: " + encrypted);
 
             socket.close();
             serverSocket.close();
+            input.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
